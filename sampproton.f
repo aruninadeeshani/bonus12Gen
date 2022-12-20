72578a6eb82e507bf32e2b002ae1b35c8ced611a
YuchunHung <yhung001@odu.edu>
2022-12-19 23:14:30 -0500

    Sampling the spectator proton with FYINT(3)*pT

--- sampproton.f
+++ sampproton.f
@@ -8,7 +8,8 @@
       INTEGER wfn,ibar,imom,rel,i,j,seed/84389723/
       logical firstr
       external ran3
-      integer*4 numCount
+      integer*4 numCount, idx, jdx
+      real*8 gammaConst1/1.5/
       
 CCCC  By default use WBArelativistic prescription with CDBONN potential  CCCC      
       
@@ -33,39 +34,27 @@ c      write(6,*) "Here - sampproton"
       ymax = 1.0
       pTmax = 1000.0
       pTmin = 1.0
-       
-      yd = 0.5
-      pT = 1.0
       numCount = 0
+      pT = 26.0
+      yd = 0.5
+    
       call dn(wfn,ibar,rel,yd,gamma,pT,FYINT,FYOFF)
-      norm = FYINT(3)
-      fyt = 0.0
+      norm = FYINT(3)*pT
       
+CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
       dowhile(fyt.LT.test)
-       test = ran3(seed)
-       yd = 5.0E-4+(5.0-1.0E-4)*ran3(seed)
-       pT = pTmin+(pTmax-pTmin)*ran3(seed)
-       call dn(wfn,ibar,rel,yd,gamma,pT,FYINT,FYOFF)
-       fyt = FYINT(3)/norm
-c        if (test > 0.88 .and. numCount < 5) then
-c            write(*,*) numCount, " test = ", test, "  fyt = ", fyt,
-c     &          "  yd = ", yd, "  pT = ", pT, " norm = ", norm,
-c     &          "  FYINT(3) = ", FYINT(3)
-c        end if
-       numCount = numCount + 1
+        test = ran3(seed)
+        yd = 5.0E-4+(5.0-1.0E-4)*ran3(seed)
+        pT = pTmin+(pTmax-pTmin)*ran3(seed)
+        call dn(wfn,ibar,rel,yd,gamma,pT,FYINT,FYOFF)
+        fyt = (FYINT(3)*pT)/norm
+c       write(*,*) numCount, (FYINT(3)*pT), norm, test, fyt
+        numCount = numCount + 1
       enddo
       write(27, *) norm, test, gamma, yD, pT, fyt, FYINT(3)
-c       write(*,*) "test = ", test, "  fyt = ", fyt
-c       write(*,*) "pT = ", pT, "  count = ", numCount
-      if (yd < 0.01) then
-        write(*,*) "Warning: small yD = ", yd,
-     &     "    pT = ", pT, "  count = ", numCount, " test = ", test
-      end if
-c            write(*,*) numCount, " test = ", test, "  fyt = ", fyt,
-c     &          "  yd = ", yd, "  pT = ", pT, " norm = ", norm,
-c     &          "  FYINT(3) = ", FYINT(3)
 
       write(*,*) "********* num Counts = ", numCount
+CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
       
 c      if(fyt.GT.1.0) write(30,*) gamma,yd,pT,fyt,test
 c      write(6,*) gamma,yd,pT,fyt,test
